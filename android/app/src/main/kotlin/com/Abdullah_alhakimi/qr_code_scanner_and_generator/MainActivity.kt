325725df027d119e3a7e1cbd0b1a2fc8e5fe1349
ABDULLAH AL-HAKIMI <abdullahalhakimi7@gmail.com>
2025-07-08 22:34:37 +0300

    improvement

--- android/app/src/main/kotlin/com/Abdullah_alhakimi/qr_code_scanner_and_generator/MainActivity.kt
+++ android/app/src/main/kotlin/com/Abdullah_alhakimi/qr_code_scanner_and_generator/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.qr_code_scanner_and_generator
+package com.Abdullah_alhakimi.qr_code_scanner_and_generator
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/Models/ad_helper.dart
+++ lib/Models/ad_helper.dart
@@ -1,18 +1,36 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+// class AdHelper {
+//   static const bool _testMode = false;
+//
+//   static String get bannerAdUnitId {
+//     if (_testMode) {
+//       return BannerAd.testAdUnitId;
+//     }
+//     return 'ca-app-pub-4734000358819500/7564695446';
+//   }
+//
+//   static String get interstitialAdUnitId {
+//     if (_testMode) {
+//       return InterstitialAd.testAdUnitId;
+//     }
+//     return 'ca-app-pub-4734000358819500/1546082008';
+//   }
+// }
+
 class AdHelper {
   static const bool _testMode = false;
 
   static String get bannerAdUnitId {
     if (_testMode) {
-      return BannerAd.testAdUnitId;
+      return 'ca-app-pub-3940256099942544/6300978111';
     }
     return 'ca-app-pub-4734000358819500/7564695446';
   }
 
   static String get interstitialAdUnitId {
     if (_testMode) {
-      return InterstitialAd.testAdUnitId;
+      return 'ca-app-pub-3940256099942544/1033173712';
     }
     return 'ca-app-pub-4734000358819500/1546082008';
   }
